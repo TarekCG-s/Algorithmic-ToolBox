b748b5c8efeceb6573dceff08a9d3d1ff5cc805c
Tarek Muhammed <tarek.3d.3dmax@gmail.com>
2018-09-03 16:15:33 +0200

    UPDATE knapsack Algorithm

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -2,10 +2,26 @@
 import sys
 
 def get_optimal_value(capacity, weights, values):
-    value = 0.
-    # write your code here
+    
+    value = 0
+    i = 0
+    
+    for x in range(len(values)):
+        values[x] = (values[x] / weights[x])
+        
+    sortedVals = sorted(values, reverse=True)
+
+    while (capacity > 0):
+        
+        index  = values.index(sortedVals[i])
+        addedWgt = min(weights[index], capacity)
+        value += (sortedVals[i] * addedWgt)
+        capacity -= addedWgt
+        i += 1                       
 
     return value
+
+
 
 
 if __name__ == "__main__":
